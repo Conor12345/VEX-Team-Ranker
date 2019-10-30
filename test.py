4a20d9cf4a6e810f15fd5fd921cb2fdec8660eea
Conor12345 <34628734+Conor12345@users.noreply.github.com>
2019-10-30 22:59:29 +0000

    emptied test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import account_management
-
-while True:
-    data = []
-    for i in range(0, 4):
-        data.append(input("Input" + str(i + 1) + ":"))
-    account_management.create_user(data[0], data[1], data[2], int(data[3]))
