8614f03c8be71b42bbba8163076bc15c0aa77a63
Conor12345 <34628734+Conor12345@users.noreply.github.com>
2019-10-30 22:56:18 +0000

    created test user accounts

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-def test(one=None, two=None, three=None, four=None):
-    print(one, two, three, four)
+import account_management
 
-test(one=2, four=2)
+while True:
+    data = []
+    for i in range(0, 4):
+        data.append(input("Input" + str(i + 1) + ":"))
+    account_management.create_user(data[0], data[1], data[2], int(data[3]))
