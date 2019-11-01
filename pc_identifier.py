0d929873a5a24a1006bed0142da99e6c87a762c7
conor <me@conorn.xyz>
2019-11-01 20:56:15 +0000

    updated window placement on laptop

--- pc_identifier.py
+++ pc_identifier.py
@@ -6,5 +6,5 @@ def getRes():
     if pc == "Desktop":
         geom = "1280x720+-1600+100"
     elif pc == "Laptop":
-        geom = "1280x720+100+100"
+        geom = "1280x720+300+150"
     return geom
