5ab58cd7769dca726bd5560b9d947fe03071991a
Conor12345 <34628734+Conor12345@users.noreply.github.com>
2019-11-05 19:59:18 +0000

    added get_team_list function

--- team_management.py
+++ team_management.py
@@ -1,7 +1,13 @@
 import sqlite3
-import api_query
 
-def check_team_presence(TeamNum): # returns true if present
+import api_query
+import match_management
+import event_management
+from importlib import reload
+
+reload(match_management)
+
+def check_team_presence(TeamNum):  # returns true if present
     db = sqlite3.connect("database.db")
     c = db.cursor()
 
@@ -9,7 +15,8 @@ def check_team_presence(TeamNum): # returns true if present
     db.commit()
     return len(results.fetchall()) == 1
 
-def import_team(TeamNum, skillRating = 0): #import team into tbl Team
+
+def import_team(TeamNum, skillRating=0):  # import team into tbl Team
     data = api_query.get_team_data(TeamNum)
     if not data:
         return False
@@ -18,6 +25,7 @@ def import_team(TeamNum, skillRating = 0): #import team into tbl Team
         c = db.cursor()
         c.execute('INSERT INTO tblTeams VALUES (?,?,?,?,?)', (TeamNum, data[0], data[1], data[2], skillRating))
         db.commit()
+
 
 def refresh_team(TeamNum):
     if not check_team_presence(TeamNum):
@@ -29,11 +37,14 @@ def refresh_team(TeamNum):
     db.commit()
     import_team(TeamNum, skill)
 
-def get_team_list():
-    db = sqlite3.connect("database.db")
-    c = db.cursor()
-    results = c.execute("SELECT TeamNum FROM tblTeams")
-    data = []
-    for team in results.fetchall():
-        data.append(team[0])
-    return data
+
+def get_team_list(EventName):
+    eventID = event_management.get_eventID(EventName)
+    matchData = match_management.import_match(eventID, True)
+    teams = []
+    for match in matchData:
+        if match["round"] == 1:
+            for team in ["red1", "red2", "red3", "blue1", "blue2", "blue3"]:
+                if match[team] != "" and match[team] not in teams:
+                    teams.append(match[team])
+    return sorted(teams)
