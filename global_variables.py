8441aa10ef595bf6b0a991880add544fe37bba6f
Conor12345 <34628734+Conor12345@users.noreply.github.com>
2019-11-05 19:31:49 +0000

    updated seasons() to return list in reverse order

--- global_variables.py
+++ global_variables.py
@@ -4,8 +4,8 @@ def text(size=20):
     return ("Verdana", size)
 
 def seasons():
-    return {"Skyrise", "Toss Up", "Sack Attack", "Gateway", "Round Up", "Clean Sweep",
-            "Elevation", "Bridge Battle", "Nothing But Net", "Starstruck", "In The Zone", "Turning Point", "Tower Takeover"}#
+    return reversed(["Skyrise", "Toss Up", "Sack Attack", "Gateway", "Round Up", "Clean Sweep",
+            "Elevation", "Bridge Battle", "Nothing But Net", "Starstruck", "In The Zone", "Turning Point", "Tower Takeover"])
 
 def countries():
     data = []
