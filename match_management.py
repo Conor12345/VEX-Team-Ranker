976e83fe2f4201cf046df01c7c8be7e94c8f177b
Conor12345 <34628734+Conor12345@users.noreply.github.com>
2019-11-05 19:37:19 +0000

    added practice flag to import match to return all match data

--- match_management.py
+++ match_management.py
@@ -4,39 +4,43 @@ import team_management
 from datetime import date
 from dateutil.relativedelta import relativedelta
 
-def import_match(EventID):
+def import_match(EventID,practice=False):
     data = api_query.get_match_data(EventID)
     if not data:
         return False
     else:
         db = sqlite3.connect("database.db")
         c = db.cursor()
-        for match in data:
-            if match["round"] != 1:
-                if match["red3"] == "":
-                    del match["red3"]
-                    del match["blue3"]
-                    del match["redsit"]
-                    del match["bluesit"]
-                else:
-                    teams = []
-                    for colour in ["blue", "red"]:
-                        team = []
-                        for teamnum in range(1,4):
-                            team.append(match[colour + str(teamnum)])
-                        teams.append(team)
-                    teams[0].remove(match["bluesit"])
-                    teams[1].remove(match["redsit"])
-                    match["blue1"] = teams[0][0]
-                    match["blue2"] = teams[0][1]
-                    match["red1"] = teams[1][0]
-                    match["red2"] = teams[1][1]
-                c.execute("INSERT INTO tblMatches VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (EventID, match["matchnum"], match["round"], match["red1"], match["red2"], match["blue1"], match["blue2"], match["redscore"], match["bluescore"]))
-                db.commit()
-                for teamNum in ["red1", "red2", "blue1", "blue2"]:
-                    if match[teamNum] != "":
-                        if not team_management.check_team_presence(match[teamNum]):
-                            team_management.import_team(match[teamNum])
+        if not practice:
+            for match in data:
+                if match["round"] != 1:
+                    if match["red3"] == "":
+                        del match["red3"]
+                        del match["blue3"]
+                        del match["redsit"]
+                        del match["bluesit"]
+                    else:
+                        teams = []
+                        for colour in ["blue", "red"]:
+                            team = []
+                            for teamnum in range(1,4):
+                                team.append(match[colour + str(teamnum)])
+                            teams.append(team)
+                        teams[0].remove(match["bluesit"])
+                        teams[1].remove(match["redsit"])
+                        match["blue1"] = teams[0][0]
+                        match["blue2"] = teams[0][1]
+                        match["red1"] = teams[1][0]
+                        match["red2"] = teams[1][1]
+                    c.execute("INSERT INTO tblMatches VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (EventID, match["matchnum"], match["round"], match["red1"], match["red2"], match["blue1"], match["blue2"], match["redscore"], match["bluescore"]))
+                    db.commit()
+                    for teamNum in ["red1", "red2", "blue1", "blue2"]:
+                        if match[teamNum] != "":
+                            if not team_management.check_team_presence(match[teamNum]):
+                                team_management.import_team(match[teamNum])
+        else:
+            return data
+
 
 def check_event_has_matches(EventID : str):
     db = sqlite3.connect("database.db")
