b6e1192847e53824280350455d5a5200f7dfda5a
Conor12345 <34628734+Conor12345@users.noreply.github.com>
2019-11-13 21:33:31 +0000

    Finished new user frames

--- screen_users.py
+++ screen_users.py
@@ -1,11 +1,14 @@
 import tkinter as tk
 
+import account_management
 import global_variables
+
 
 class NewUser(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
-        #TODO make new user frame
+
+        self.parent = parent
 
         nameLabel = tk.Label(self, text="New user", font=global_variables.text(16))
         nameLabel.grid(row=0, column=0, columnspan=2)
@@ -17,8 +20,37 @@ class NewUser(tk.Frame):
             array = [tk.Label(self, text=labels[i], font=global_variables.text(12))]
             array[0].grid(row=i + 1, column=0)
 
-            array.append(tk.Entry(self, font=global_variables.text(12)))
-            array[1].grid(row=i + 1, column=1)
+            if i == 4:
+                self.AdminVar = tk.IntVar()
+                array.append(tk.Checkbutton(self, variable=self.AdminVar))
+                array[-1].grid(row=i + 1, column=1)
+
+            elif i == 1 or i == 2:
+                array.append(tk.Entry(self, font=global_variables.text(12), show="*"))
+                array[-1].grid(row=i + 1, column=1)
+
+            else:
+                array.append(tk.Entry(self, font=global_variables.text(12)))
+                array[-1].grid(row=i + 1, column=1)
+
+            self.entryBoxes.append(array)
+
+        self.buttons = [tk.Button(self, text="Create User", font=global_variables.text(16), command=self.newUserCreate)]
+        self.buttons[0].grid(row=6, column=0, columnspan=2)
+        
+    def newUserCreate(self):
+        if self.entryBoxes[1][1].get() != self.entryBoxes[2][1].get():
+            errorLabel = tk.Label(self, text="ERROR - Passwords do not match", font=global_variables.text(12))
+            errorLabel.grid(row=7, column=0, columnspan=2)
+
+        elif account_management.create_user(self.entryBoxes[0][1].get(), self.entryBoxes[1][1].get(), self.entryBoxes[3][1].get(), self.AdminVar.get()):
+            self.parent.show_users()
+
+        else:
+            errorLabel = tk.Label(self, text="ERROR - Specified Team does not exist", font=global_variables.text(12))
+            errorLabel.grid(row=7, column=0, columnspan=2)
+
+            
 
 
 #TODO modify new user frame to work as update user
