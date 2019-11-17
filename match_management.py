7552b6e0da3e4d8d6006b2168c70291498332c7a
Conor12345 <34628734+Conor12345@users.noreply.github.com>
2019-11-17 16:05:57 +0000

    reformatted file

--- match_management.py
+++ match_management.py
@@ -4,7 +4,7 @@ import api_query
 import team_management
 
 
-def import_match(EventID,practice=False):
+def import_match(EventID, practice=False):
     data = api_query.get_match_data(EventID)
     if not data:
         return False
@@ -23,7 +23,7 @@ def import_match(EventID,practice=False):
                         teams = []
                         for colour in ["blue", "red"]:
                             team = []
-                            for teamnum in range(1,4):
+                            for teamnum in range(1, 4):
                                 team.append(match[colour + str(teamnum)])
                             teams.append(team)
                         teams[0].remove(match["bluesit"])
@@ -32,7 +32,9 @@ def import_match(EventID,practice=False):
                         match["blue2"] = teams[0][1]
                         match["red1"] = teams[1][0]
                         match["red2"] = teams[1][1]
-                    c.execute("INSERT INTO tblMatches VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (EventID, match["matchnum"], match["round"], match["red1"], match["red2"], match["blue1"], match["blue2"], match["redscore"], match["bluescore"]))
+                    c.execute("INSERT INTO tblMatches VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                              (EventID, match["matchnum"], match["round"], match["red1"], match["red2"],
+                               match["blue1"], match["blue2"], match["redscore"], match["bluescore"]))
                     db.commit()
                     for teamNum in ["red1", "red2", "blue1", "blue2"]:
                         if match[teamNum] != "":
@@ -42,7 +44,7 @@ def import_match(EventID,practice=False):
             return data
 
 
-def check_event_has_matches(EventID : str):
+def check_event_has_matches(EventID: str):
     db = sqlite3.connect("database.db")
     c = db.cursor()
     results = c.execute("SELECT * FROM tblMatches WHERE EventID=(?)", (EventID,))
