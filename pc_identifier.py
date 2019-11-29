d5f50c896de248941bcae573bcce6ac731bd6997
conor1 <me@conorn.xyz>
2019-11-29 13:18:55 +0000

    updated pc identifier to include old laptop

--- pc_identifier.py
+++ pc_identifier.py
@@ -10,6 +10,10 @@ def getRes():
         geom = "1920x1080+-1920+0"
     elif pc == "Laptop":
         geom = "1920x1080+0+0"
+    elif pc == "BadLaptop":
+        geom = "1366x768+0+0"
+    else:
+        geom = "1920x720+0+0"
     return geom
 
 def getType():
@@ -17,5 +21,7 @@ def getType():
     if pc == "Desktop":
         return "zoomed"
     elif pc == "Laptop":
+        return "normal"
+    elif pc == "BadLaptop":
         return "normal"
 
